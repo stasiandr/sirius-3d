e65113242e0ed91469c6720482685c4b383651fb
VorakT <v0rak@mail.ru>
2021-12-17 12:59:42 +0300

    fix: var_names

--- Assets/Scripts/MeshTools/SphereGenerator.cs
+++ Assets/Scripts/MeshTools/SphereGenerator.cs
@@ -6,7 +6,7 @@ namespace MeshTools
 {
     public static class SphereGenerator
     {
-        public static MyMesh GenerateSphere(int parallels, int medians)
+        public static MyMesh GenerateSphere(int parallels, int meridians)
         {
             var mesh = new MyMesh
             {
@@ -18,9 +18,9 @@ namespace MeshTools
             for (int i = 1; i < parallels; ++i)
             {
                 var phi = System.Math.PI * i / parallels;
-                for (int j = 0; j < medians; ++j)
+                for (int j = 0; j < meridians; ++j)
                 {
-                    var theta = 2 * System.Math.PI * j / medians;
+                    var theta = 2 * System.Math.PI * j / meridians;
                     var x = (float)(System.Math.Sin(phi) * System.Math.Cos(theta));
                     var y = (float)(System.Math.Cos(phi));
                     var z = (float)(System.Math.Sin(phi) * System.Math.Sin(theta));
@@ -29,27 +29,27 @@ namespace MeshTools
             }
             mesh.Vertices.Add(new Vector3(0, -1, 0));
 
-            var topVertex = 0;
-            var bottomVertex = 1 + (parallels - 1) * medians;
-            for (int i = 0; i < medians; ++i)
+            var topVertice = 0;
+            var bottomVertice = 1 + (parallels - 1) * meridians;
+            for (int i = 0; i < meridians; ++i)
             {
                 var i0 = i + 1;
-                var i1 = (i + 1) % medians + 1;
-                mesh.AddTriangle(topVertex, i1, i0);
-                i0 = i + medians * (parallels - 2) + 1;
-                i1 = (i + 1) % medians + medians * (parallels - 2) + 1;
-                mesh.AddTriangle(bottomVertex, i0, i1);
+                var i1 = (i + 1) % meridians + 1;
+                mesh.AddTriangle(topVertice, i1, i0);
+                i0 = i + meridians * (parallels - 2) + 1;
+                i1 = (i + 1) % meridians + meridians * (parallels - 2) + 1;
+                mesh.AddTriangle(bottomVertice, i0, i1);
             }
 
             for (int j = 0; j < parallels - 2; ++j)
             {
-                var j0 = j * medians + 1;
-                var j1 = (j + 1) * medians + 1;
-                for (int i = 0; i < medians; ++i)
+                var j0 = j * meridians + 1;
+                var j1 = (j + 1) * meridians + 1;
+                for (int i = 0; i < meridians; ++i)
                 {
                     var i0 = j0 + i;
-                    var i1 = j0 + (i + 1) % medians;
-                    var i2 = j1 + (i + 1) % medians;
+                    var i1 = j0 + (i + 1) % meridians;
+                    var i2 = j1 + (i + 1) % meridians;
                     var i3 = j1 + i;
                     mesh.AddTriangle(i0, i1, i3);
                     mesh.AddTriangle(i1, i2, i3);
