a8daa4e3cd7d3b27990680828ae219ccfbd8aa39
romchikk19 <66239294+romchikk19@users.noreply.github.com>
2021-12-17 12:34:02 +0300

    upd: primitives params

--- Assets/Scripts/Commands/CreatePrimitiveCommand.cs
+++ Assets/Scripts/Commands/CreatePrimitiveCommand.cs
@@ -1,16 +1,24 @@
 using System;
 using MeshTools;
 using SceneProvider;
+using UnityEngine;
 
 namespace Commands
 {
     public struct CreatePrimitiveCommand : ICommand
     {
         public string MeshType;
-
+        public Vector3 Pos;
+        public Vector3 Scale;
+        public int Details;
+        public int Details2;
         public CreatePrimitiveCommand(string meshType)
         {
             MeshType = meshType;
+            Pos = Vector3.zero;
+            Scale = Vector3.one;
+            Details = 10;
+            Details2 = 10;
         }
 
         public void Apply()
@@ -18,23 +26,23 @@ namespace Commands
             MyMesh mesh;
             switch (MeshType) {
                 case "Cube":
-                    mesh = MeshGenerator.GenerateCube();
+                    mesh = MeshGenerator.GenerateCube(/*Pos, Scale*/);
                     break;
                 case "Sphere":
                     throw new NotImplementedException();
-                    //mesh = MeshGenerator.GenerateSphere();
+                    //mesh = MeshGenerator.GenerateSphere(Pos, Scale.x);
                     //break;
                 case "Cone":
                     throw new NotImplementedException();
-                    //mesh = MeshGenerator.GenerateCone();
+                    //mesh = MeshGenerator.GenerateCone(Pos, Scale.x, Scale.y, Details);
                     //break;
                 case "Cylinder":
                     throw new NotImplementedException();
-                    //mesh = MeshGenerator.GenerateCylinder();
+                    //mesh = MeshGenerator.GenerateCylinder(Pos, Scale.x, Scale.y, Details);
                     //break;
                 case "Torus":
                     throw new NotImplementedException();
-                    //mesh = MeshGenerator.GenerateTprus();
+                    //mesh = MeshGenerator.GenerateTorus(Pos, Scale.x, Scale.y, Scale.z, Details1, Details2);
                     //break;
                 default:
                     throw new NotImplementedException();
