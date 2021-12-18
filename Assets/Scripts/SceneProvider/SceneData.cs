c42b8917b9aa3200c3f577e0311766ce0b0842a1
hikkiyomi <hg-4ybak@mail.ru>
2021-12-18 11:08:42 +0300

    fix: selector

--- Assets/Scripts/SceneProvider/SceneData.cs
+++ Assets/Scripts/SceneProvider/SceneData.cs
@@ -26,7 +26,7 @@ namespace SceneProvider
             
             Targets = new List<GameObject>();
 
-            foreach (var col in obj)
+            foreach (var col in obj.Where(col => col != null))
             {
                 Targets.Add(col.gameObject);
             }
