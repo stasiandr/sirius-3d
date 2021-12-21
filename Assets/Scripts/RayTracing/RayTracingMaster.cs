1291ccd142ce6fc9c409390b737fdf87e2a89c3e
romchikk19 <66239294+romchikk19@users.noreply.github.com>
2021-12-21 10:40:05 +0300

    everything black

--- Assets/Scripts/RayTracing/RayTracingMaster.cs
+++ Assets/Scripts/RayTracing/RayTracingMaster.cs
@@ -16,7 +16,6 @@ public class RayTracingMaster : MonoBehaviour
     public uint SpheresMax = 100;
     public float SpherePlacementRadius = 100.0f;
     private ComputeBuffer _sphereBuffer;
-    private ComputeBuffer _triangleBuffer;
     struct Sphere
     {
         public Vector3 position;
@@ -25,6 +24,15 @@ public class RayTracingMaster : MonoBehaviour
         public Vector3 specular;
     };
 
+    private ComputeBuffer _triangleBuffer;
+    struct Triangle
+    {
+        public Vector3 v0;
+        public Vector3 v1;
+        public Vector3 v2;
+        public Vector3 albedo;
+        public Vector3 specular;
+    };
     private void OnEnable()
     {
         //_currentSample = 0;
@@ -35,8 +43,10 @@ public class RayTracingMaster : MonoBehaviour
         if (_sphereBuffer != null)
             _sphereBuffer.Release();
     }
+    public int SphereSeed;
     private void SetUpScene()
     {
+        Random.InitState(SphereSeed);
         List<Sphere> spheres = new List<Sphere>();
         // Add a number of random spheres
         for (int i = 0; i < SpheresMax; i++)
@@ -55,7 +65,7 @@ public class RayTracingMaster : MonoBehaviour
             }
             // Albedo and specular color
             Color color = Random.ColorHSV();
-            bool metal = Random.value < 0.5f;
+            bool metal = Random.value < 0.0f;
             sphere.albedo = metal ? Vector3.zero : new Vector3(color.r, color.g, color.b);
             sphere.specular = metal ? new Vector3(color.r, color.g, color.b) : Vector3.one * 0.04f;
             // Add the sphere to the list
@@ -66,9 +76,16 @@ public class RayTracingMaster : MonoBehaviour
         // Assign to compute buffer
         _sphereBuffer = new ComputeBuffer(spheres.Count, 40);
         _sphereBuffer.SetData(spheres);
-        foreach (var obj in 
-        _triangleBuffer = new ComputeBuffer(spheres.Count, 40);
-        _triangleBuffer.SetData(spheres);
+        List<Triangle> triangles = new List<Triangle>();
+        var T = new Triangle();
+        T.v0 = new Vector3(0, 0, 0);
+        T.v1 = new Vector3(50, 100, 0);
+        T.v2 = new Vector3(100, 0, 0);
+        T.albedo = new Vector3(0.6f, 0.5f, 0.2f);
+        T.specular = new Vector3(0.09f, 0.05f, 0.04f);
+        triangles.Add(T);
+        _triangleBuffer = new ComputeBuffer(triangles.Count, 60);
+        _triangleBuffer.SetData(triangles);
     }
     private void Awake()
     {
@@ -80,21 +97,35 @@ public class RayTracingMaster : MonoBehaviour
         Render(destination);
     }
 
-    private void SetShaderParameters()
+    private uint _currentSample = 0;
+    private void Update()
+    {
+        if (transform.hasChanged)
+        {
+            _currentSample = 0;
+            transform.hasChanged = false;
+        }
+    }
+
+        private void SetShaderParameters()
     {
         Vector3 l = DirectionalLight.transform.forward;
+        RayTracingShader.SetVector("_PixelOffset", new Vector2(Random.value, Random.value));
         RayTracingShader.SetVector("_DirectionalLight", new Vector4(l.x, l.y, l.z, DirectionalLight.intensity));
         RayTracingShader.SetTexture(0, "_SkyboxTexture", SkyboxTexture);
         RayTracingShader.SetMatrix("_CameraToWorld", _camera.cameraToWorldMatrix);
         RayTracingShader.SetMatrix("_CameraInverseProjection", _camera.projectionMatrix.inverse);
         RayTracingShader.SetBuffer(0, "_Spheres", _sphereBuffer);
+        RayTracingShader.SetBuffer(0, "_Triangles", _triangleBuffer);
     }
+
+    private RenderTexture _converged;
+    private Material _addMaterial;
 
     private void Render(RenderTexture destination)
     {
         // Make sure we have a current render target
         InitRenderTexture();
-
         // Set the target and dispatch the compute shader
         RayTracingShader.SetTexture(0, "Result", _target);
         int threadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
@@ -102,13 +133,19 @@ public class RayTracingMaster : MonoBehaviour
         RayTracingShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
 
         // Blit the result texture to the screen
-        Graphics.Blit(_target, destination);
+        if (_addMaterial == null)
+            _addMaterial = new Material(Shader.Find("Hidden/AddShader"));
+        _addMaterial.SetFloat("_Sample", _currentSample);
+        Graphics.Blit(_target, _converged, _addMaterial);
+        Graphics.Blit(_converged, destination);
+        _currentSample++;
     }
 
     private void InitRenderTexture()
     {
         if (_target == null || _target.width != Screen.width || _target.height != Screen.height)
         {
+            _currentSample = 0;
             // Release render texture if we already have one
             if (_target != null)
                 _target.Release();
@@ -118,6 +155,15 @@ public class RayTracingMaster : MonoBehaviour
                 RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
             _target.enableRandomWrite = true;
             _target.Create();
+        }
+        if (_converged == null || _converged.width != Screen.width || _converged.height != Screen.height)
+        {
+            if (_converged != null)
+                _converged.Release();
+            _converged = new RenderTexture(Screen.width, Screen.height, 0,
+                RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            _converged.enableRandomWrite = true;
+            _converged.Create();
         }
     }
 }
