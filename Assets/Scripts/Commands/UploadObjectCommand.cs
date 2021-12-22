897e3f2446dcf360a5666cdc3ba2c05ba57764de
romchikk19 <66239294+romchikk19@users.noreply.github.com>
2021-12-22 12:03:31 +0300

    upd: multimesh obj upload

--- Assets/Scripts/Commands/UploadObjectCommand.cs
+++ Assets/Scripts/Commands/UploadObjectCommand.cs
@@ -13,11 +13,31 @@ namespace Commands
             SceneData.UploadedMesh = new MyMesh();
             string path = FilePath();
             var obj = new Dummiesman.OBJLoader().Load(path);
-            var mesh = obj.GetComponent<MeshFilter>().mesh;
-            foreach (var ver in mesh.vertices)
-                SceneData.UploadedMesh.Vertices.Add(ver);
-            foreach (var tr in mesh.triangles)
-                SceneData.UploadedMesh.Triangles.Add(tr);
+            if (obj.GetComponent<MeshFilter>())
+            {
+                var mesh = obj.GetComponent<MeshFilter>().mesh;
+                SceneData.UploadedMesh.Vertices = new List<Vector3>();
+                foreach (var ver in mesh.vertices)
+                    SceneData.UploadedMesh.Vertices.Add(ver);
+                SceneData.UploadedMesh.Triangles = new List<int>();
+                foreach (var tr in mesh.triangles)
+                    SceneData.UploadedMesh.Triangles.Add(tr);
+            }
+            else
+            {
+                SceneData.UploadedMesh.Vertices = new List<Vector3>();
+                SceneData.UploadedMesh.Triangles = new List<int>();
+                int N = 0;
+                foreach (Transform obj2 in obj.transform)
+                {
+                    var mesh = obj2.gameObject.GetComponent<MeshFilter>().mesh;
+                    foreach (var ver in mesh.vertices)
+                        SceneData.UploadedMesh.Vertices.Add(ver);
+                    foreach (var tr in mesh.triangles)
+                        SceneData.UploadedMesh.Triangles.Add(tr + N);
+                    N += mesh.vertices.Length;
+                }
+            }
             GameObject.Destroy(obj);
         }
 
